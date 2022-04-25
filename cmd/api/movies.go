39b7c586cf4d784d989ef13981d199528df2fcc9
Marco <marco.wettstein@gmail.com>
2022-04-25 14:58:15 +0200

    parse runtime in form of "x mins"

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -10,10 +10,10 @@ import (
 
 func (app *application) createMovieHandler(writer http.ResponseWriter, request *http.Request) {
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string          `json:"title"`
+		Year    int32           `json:"year"`
+		Runtime data.RuntimeMin `json:"runtime"`
+		Genres  []string        `json:"genres"`
 	}
 
 	err := app.readJSON(writer, request, &input)
--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,14 +1,38 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 type RuntimeMin int32
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 func (r RuntimeMin) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 	quotedJSONValue := strconv.Quote(jsonValue)
 	return []byte(quotedJSONValue), nil
+}
+
+func (r *RuntimeMin) UnmarshalJSON(data []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(data))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	splitted := strings.Split(unquotedJSONValue, " ")
+	if len(splitted) != 2 || splitted[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	runtimeInMin, err := strconv.ParseInt(splitted[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = RuntimeMin(runtimeInMin)
+	return nil
 }

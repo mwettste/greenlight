09d47d805fe9f7642984111777e2ec90252251fd
Marco <marco.wettstein@gmail.com>
2022-04-27 21:18:28 +0200

    allow patch updates

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -95,10 +95,10 @@ func (app *application) updateMovieHandler(writer http.ResponseWriter, request *
 	}
 
 	var input struct {
-		Title   string          `json:"title"`
-		Year    int32           `json:"year"`
-		Runtime data.RuntimeMin `json:"runtime"`
-		Genres  []string        `json:"genres"`
+		Title   *string          `json:"title"`
+		Year    *int32           `json:"year"`
+		Runtime *data.RuntimeMin `json:"runtime"`
+		Genres  []string         `json:"genres"`
 	}
 
 	err = app.readJSON(writer, request, &input)
@@ -107,10 +107,21 @@ func (app *application) updateMovieHandler(writer http.ResponseWriter, request *
 		return
 	}
 
-	movie.Title = input.Title
-	movie.Year = input.Year
-	movie.Genres = input.Genres
-	movie.RuntimeMin = input.Runtime
+	if input.Title != nil {
+		movie.Title = *input.Title
+	}
+
+	if input.Year != nil {
+		movie.Year = *input.Year
+	}
+
+	if input.Genres != nil {
+		movie.Genres = input.Genres
+	}
+
+	if input.Runtime != nil {
+		movie.RuntimeMin = *input.Runtime
+	}
 
 	v := validator.New()
 

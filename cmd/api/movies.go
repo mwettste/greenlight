77d098b726b72ec5014e8fee539aa05ad8dcbd71
Marco <marco.wettstein@gmail.com>
2022-04-27 18:05:59 +0200

    allow updating of movies

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -75,3 +75,58 @@ func (app *application) showMovieHandler(writer http.ResponseWriter, request *ht
 		app.serverErrorResponse(writer, request, err)
 	}
 }
+
+func (app *application) updateMovieHandler(writer http.ResponseWriter, request *http.Request) {
+	id, err := app.readIDParameter(request)
+	if err != nil {
+		app.notFoundResponse(writer, request)
+		return
+	}
+
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(writer, request)
+		default:
+			app.serverErrorResponse(writer, request, err)
+		}
+		return
+	}
+
+	var input struct {
+		Title   string          `json:"title"`
+		Year    int32           `json:"year"`
+		Runtime data.RuntimeMin `json:"runtime"`
+		Genres  []string        `json:"genres"`
+	}
+
+	err = app.readJSON(writer, request, &input)
+	if err != nil {
+		app.badRequestResponse(writer, request, err)
+		return
+	}
+
+	movie.Title = input.Title
+	movie.Year = input.Year
+	movie.Genres = input.Genres
+	movie.RuntimeMin = input.Runtime
+
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(writer, request, v.Errors)
+		return
+	}
+
+	err = app.models.Movies.Update(movie)
+	if err != nil {
+		app.serverErrorResponse(writer, request, err)
+		return
+	}
+
+	err = app.writeJSON(writer, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(writer, request, err)
+	}
+}

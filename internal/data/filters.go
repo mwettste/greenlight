69c461e6ae474844c0557828324a0d8078c97974
Marco Wettstein <marco.wettstein@gmail.com>
2022-05-15 17:52:49 +0200

    fix usage of validator after rewriting it with generics

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -30,7 +30,7 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 
 	// Check that the sort parameter matches a value in the safelist.
-	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+	v.Check(validator.PermittedValue(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
 }
 
 func (f Filters) sortColumn() string {

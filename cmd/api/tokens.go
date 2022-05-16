426f41763ba61f49703cd4bae00d9fae9ed72dbb
Marco Wettstein <marco.wettstein@gmail.com>
2022-05-16 20:13:21 +0200

    allow creating authentication tokens

---
+++ cmd/api/tokens.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/mwettste/greenlight/internal/data"
+	"github.com/mwettste/greenlight/internal/validator"
+)
+
+func (app *application) createAuthenticationTokenHandler(writer http.ResponseWriter, request *http.Request) {
+	var input struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := app.readJSON(writer, request, &input)
+	if err != nil {
+		app.badRequestResponse(writer, request, err)
+		return
+	}
+
+	v := validator.New()
+
+	data.ValidateEmail(v, input.Email)
+	data.ValidatePasswordPlaintext(v, input.Password)
+
+	if !v.Valid() {
+		app.failedValidationResponse(writer, request, v.Errors)
+		return
+	}
+
+	user, err := app.models.Users.GetByEmail(input.Email)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.invalidCredentialsResponse(writer, request)
+		default:
+			app.serverErrorResponse(writer, request, err)
+		}
+		return
+	}
+
+	match, err := user.Password.Matches(input.Password)
+	if err != nil {
+		app.serverErrorResponse(writer, request, err)
+		return
+	}
+
+	if !match {
+		app.invalidCredentialsResponse(writer, request)
+		return
+	}
+
+	token, err := app.models.Tokens.New(user.ID, 24*time.Hour, data.ScopeAuthentication)
+	if err != nil {
+		app.serverErrorResponse(writer, request, err)
+		return
+	}
+
+	err = app.writeJSON(writer, http.StatusCreated, envelope{"authentication_token": token}, nil)
+	if err != nil {
+		app.serverErrorResponse(writer, request, err)
+	}
+}
